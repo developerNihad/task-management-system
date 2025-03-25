4c465f7fb9cdbe5aaf0787a1a3d4d2965c56b0c4
developerNihad <nihadnahmatli@gmail.com>
2025-03-25 21:24:05 +0400

    changed

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,13 @@
+from django.contrib import admin
 from django.urls import path
 from users.views import RegisterView, LoginView, LogoutView
+from tasks.views import TaskListCreateView, TaskDetailView
 
 urlpatterns = [
+    path("admin/", admin.site.urls),
     path("api/register/", RegisterView.as_view(), name='register'),
     path("api/login/", LoginView.as_view(), name='login'),
     path("api/logout/", LogoutView.as_view(), name='logout'),
+    path("api/tasks/", TaskListCreateView.as_view(), name='task-list'),
+    path("api/tasks/<int:pk>/", TaskDetailView.as_view(), name='task-detail'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from .models import Task
+from .serializers import TaskSerializer
 
-# Create your views here.
+
+class TaskListCreateView(generics.ListCreateAPIView):
+
+    serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user)
+    
+
+class TaskDetailView(generics.RetrieveUpdateDestroyAPIView):
+    
+    serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user)
